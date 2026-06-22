138b399b6191160742e69531366f05c94f05b501
gothsins <gui.starkteam@gmail.com>
2026-06-22 00:20:21 -0300

    chore: initial project setup

---
+++ src/main/java/com/guilherme/finance_batch/FinanceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.finance_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/finance_batch/FinanceBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.finance_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
